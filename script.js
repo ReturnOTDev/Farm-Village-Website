aea8939c7ec6c8a15a48a8e5806858a703e4a94f
David Shayne <dmshayne@gmail.com>
2019-10-17 12:21:50 +0200

    responsive gallery

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const carousel = () => {
 	const carouselSlide = document.querySelector(".carousel-slide");
 	const carouselImages = document.querySelectorAll(".carousel-slide img");
+	const carouselContainer = document.querySelector(".carousel-container");
 
 	// Buttons
 	const prevBtn = document.querySelector("#prevBtn");
@@ -12,6 +13,7 @@ const carousel = () => {
 
 	const size = carouselImages[0].clientWidth;
 
+	carouselContainer.style.width = `${size}px`;
 	carouselSlide.style.transform = `translateX(${-size * counter}px)`;
 
 	// Button Listeners
@@ -45,52 +47,56 @@ const carousel = () => {
 
 carousel();
 
-const roomOptions = document.querySelectorAll(".room-option");
-const roomCarousel = document.querySelector(".room-carousel");
-const roomCarouselContainer = document.querySelector(
-	".room-carousel-container"
-);
+const carouselRoom = () => {
+	const roomOptions = document.querySelectorAll(".room-option");
+	const roomCarousel = document.querySelector(".room-carousel");
+	const roomCarouselContainer = document.querySelector(
+		".room-carousel-container"
+	);
 
-// Buttons
-const next = document.querySelector("#roomNext");
-const prev = document.querySelector("#roomPrev");
-const size = roomOptions[0].clientWidth;
+	// Buttons
+	const next = document.querySelector("#roomNext");
+	const prev = document.querySelector("#roomPrev");
+	const size = roomOptions[0].clientWidth;
 
-// Counter
-let counter = 1;
+	// Counter
+	let counter = 1;
 
-roomCarouselContainer.style.width = `${size}px`;
-roomCarousel.style.transform = `translateX(-${size}px)`;
+	roomCarouselContainer.style.width = `${size}px`;
+	roomCarousel.style.transform = `translateX(-${size}px)`;
 
-// function to move the slider
-const move = num => {
-	roomCarousel.style.transition = "transform 0.8s ease-in-out";
-	roomOptions[counter].style.animation = "fadeOut 0.8s ease-in-out";
-	counter += num;
-	roomCarousel.style.transform = `translateX(-${size * counter}px)`;
-	roomOptions[counter].style.animation = "none";
+	// function to move the slider
+	const move = num => {
+		roomCarousel.style.transition = "transform 0.8s ease-in-out";
+		roomOptions[counter].style.animation = "fadeOut 0.8s ease-in-out";
+		counter += num;
+		roomCarousel.style.transform = `translateX(-${size * counter}px)`;
+		roomOptions[counter].style.animation = "none";
+	};
+
+	// Event Listeners
+	next.addEventListener("click", () => {
+		move(1);
+	});
+	prev.addEventListener("click", () => {
+		move(-1);
+	});
+
+	// Resetting of slider when reaching end
+	roomCarousel.addEventListener("transitionend", () => {
+		if (counter > roomOptions.length - 2) {
+			roomCarousel.style.transition = "none";
+			counter = 1;
+			roomOptions[counter].style.animation = "none";
+			roomCarousel.style.transform = `translateX(-${size * counter}px)`;
+		}
+		if (counter <= 0) {
+			roomCarousel.style.transition = "none";
+			counter = roomOptions.length - 2;
+			roomOptions[counter].style.animation = "none";
+			roomCarousel.style.transform = `translateX(-${size * counter}px)`;
+		}
+	});
 };
 
-// Event Listeners
-next.addEventListener("click", () => {
-	move(1);
-});
-prev.addEventListener("click", () => {
-	move(-1);
-});
-
-// Resetting of slider when reaching end
-roomCarousel.addEventListener("transitionend", () => {
-	if (counter > roomOptions.length - 2) {
-		roomCarousel.style.transition = "none";
-		counter = 1;
-		roomOptions[counter].style.animation = "none";
-		roomCarousel.style.transform = `translateX(-${size * counter}px)`;
-	}
-	if (counter <= 0) {
-		roomCarousel.style.transition = "none";
-		counter = roomOptions.length - 2;
-		roomOptions[counter].style.animation = "none";
-		roomCarousel.style.transform = `translateX(-${size * counter}px)`;
-	}
-});
+carouselRoom();
